5798288e6586eb0692884b5f548149e9dfdde67b
jutsodev <jutsodev@users.noreply.github.com>
2026-05-29 05:54:20 +0000

    fix: restore ContextMenuActionItem action signature style

--- submodules/TelegramUI/Sources/ChatInterfaceStateContextMenus.swift
+++ submodules/TelegramUI/Sources/ChatInterfaceStateContextMenus.swift
@@ -1915,7 +1915,7 @@ func contextMenuForChatPresentationInterfaceState(chatPresentationInterfaceState
         if UserDefaults.standard.bool(forKey: "MQGram.readUntilMessage") {
             actions.append(.action(ContextMenuActionItem(text: chatPresentationInterfaceState.strings.ChatList_Context_MarkAsRead, icon: { theme in
                 return generateTintedImage(image: UIImage(bundleImageName: "Chat/Context Menu/MarkAsUnread"), color: theme.actionSheet.primaryTextColor)
-            }, iconSource: nil, action: { f in
+            }, iconSource: nil, action: { _, f in
                 f(.dismissWithoutContent)
                 let _ = (context.engine.messages.markAllAsRead(peerId: message.id.peerId, upperIndex: message.index)
                 |> deliverOnMainQueue).startStandalone()
@@ -1927,8 +1927,8 @@ func contextMenuForChatPresentationInterfaceState(chatPresentationInterfaceState
             if editHistory != nil {
                 actions.append(.action(ContextMenuActionItem(text: "История редактирований", icon: { theme in
                     return generateTintedImage(image: UIImage(bundleImageName: "Chat/Context Menu/MessageDelete"), color: theme.actionSheet.primaryTextColor)
-                }, iconSource: nil, action: { action in
-                    action.dismissWithResult(.dismissWithoutContent)
+                }, iconSource: nil, action: { _, f in
+                    f(.dismissWithoutContent)
                 })))
             }
         }
